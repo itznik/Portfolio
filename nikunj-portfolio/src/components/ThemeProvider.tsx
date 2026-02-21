c0472024c9bd3c60d6b82802cbe4ddd05a5794b1
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-02-22 02:05:15 +0530

    Update ThemeProvider.tsx

--- nikunj-portfolio/src/components/ThemeProvider.tsx
+++ nikunj-portfolio/src/components/ThemeProvider.tsx
@@ -15,7 +15,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
       {children}
     </NextThemesProvider>
   );
