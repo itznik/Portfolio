c5aa8a8c12594222ea882cc132edea46d36262d0
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-02-22 03:26:12 +0530

    Update robots.ts

--- nikunj-portfolio/src/app/robots.ts
+++ nikunj-portfolio/src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://your-live-domain.com/sitemap.xml',
+    sitemap: 'https://nikunjvariya.vercel.app/sitemap.xml',
   }
 }
