74eb44b07413e215cca545950dbd2187d0980808
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-02-28 14:51:49 +0530

    Update page.tsx

--- nikunj-portfolio/src/app/page.tsx
+++ nikunj-portfolio/src/app/page.tsx
@@ -7,7 +7,7 @@ import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full relative overflow-clip">
+    <div className="flex flex-col w-full relative overflow-clip print:hidden">
       
       {/* The fully fixed, responsive Nav */}
       <Navbar />
